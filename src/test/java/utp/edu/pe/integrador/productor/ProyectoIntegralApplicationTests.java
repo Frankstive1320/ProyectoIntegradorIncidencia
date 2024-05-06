1d18d656be7806a98046655a50525fda9462f9b1
OscarCantoral <52679085+OscarCantoral@users.noreply.github.com>
2024-05-06 12:20:00 -0500

    Create ProyectoIntegralApplicationTests.java

---
+++ src/test/java/utp/edu/pe/integrador/productor/ProyectoIntegralApplicationTests.java
@@ -0,0 +1,13 @@
+package utp.edu.pe.integrador.productor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
