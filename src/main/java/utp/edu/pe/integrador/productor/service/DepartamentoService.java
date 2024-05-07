ce1f5a133ab102fec89b2276fb4b7e95472f3150
OscarCantoral <52679085+OscarCantoral@users.noreply.github.com>
2024-05-07 00:40:23 -0500

    Create DepartamentoService.java

---
+++ src/main/java/utp/edu/pe/integrador/productor/service/DepartamentoService.java
@@ -0,0 +1,35 @@
+package utp.edu.pe.integrador.productor.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import utp.edu.pe.integrador.productor.interfaces.IAverias;
+import utp.edu.pe.integrador.productor.interfaces.IDepartamento;
+import utp.edu.pe.integrador.productor.interfacesservice.IDepartamentoService;
+import utp.edu.pe.integrador.productor.model.Departamento;
+
+import java.util.List;
+
+public class DepartamentoService implements IDepartamentoService {
+
+    @Autowired
+    private IDepartamento departamento1;
+
+    @Override
+    public List<Departamento> listarDepartamentos() {
+        return (List<Departamento>) departamento1.findAll();
+    }
+
+    @Override
+    public List<Departamento> listarDepartamentosActivos() {
+        return (List<Departamento>) departamento1.findAll();
+    }
+
+    @Override
+    public void ActivarDepartamento(int idDepartamento) {
+        departamento1.ActivarDepartamento(idDepartamento);
+    }
+
+    @Override
+    public void DesactivarDepartamento(int idDepartamento) {
+        departamento1.DesactivarDepartamento(idDepartamento);
+    }
+}
