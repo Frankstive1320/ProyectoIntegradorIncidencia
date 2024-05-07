66658a099b29cc0f9835a880746f0d4b8116604c
OscarCantoral <52679085+OscarCantoral@users.noreply.github.com>
2024-05-07 00:40:29 -0500

    Create IDepartamentoService.java

---
+++ src/main/java/utp/edu/pe/integrador/productor/interfacesservice/IDepartamentoService.java
@@ -0,0 +1,16 @@
+package utp.edu.pe.integrador.productor.interfacesservice;
+
+import utp.edu.pe.integrador.productor.model.Departamento;
+
+import java.util.List;
+
+public interface IDepartamentoService {
+
+    public List<Departamento> listarDepartamentos();
+
+    public List<Departamento> listarDepartamentosActivos();
+
+    public void ActivarDepartamento(int idDepartamento);
+
+    public void DesactivarDepartamento(int idDepartamento);
+}
