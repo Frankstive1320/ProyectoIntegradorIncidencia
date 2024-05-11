35ce1b44aa69641bc02e21e7fd10e409f47933ef
OscarCantoral <52679085+OscarCantoral@users.noreply.github.com>
2024-05-11 16:38:11 -0500

    Update IProvincia.java

--- src/main/java/utp/edu/pe/integrador/productor/interfaces/IProvincia.java
+++ src/main/java/utp/edu/pe/integrador/productor/interfaces/IProvincia.java
@@ -14,4 +14,15 @@ public interface IProvincia extends CrudRepository<Provincia, Integer> {
 
     @Query(value ="select * from provincia where estado_provincia = 'ACTIVO'", nativeQuery = true)
     public List<Provincia> listarProvinciasActivos();
+
+    String query2=
+            "SELECT T1.* FROM provincia T1 " +
+            "INNER JOIN departamento T2 ON " +
+            "T1.cod_departamento=T2.cod_departamento " +
+            "WHERE " +
+            "T2.estado_departamento='ACTIVO' AND " +
+            "T1.estado_provincia='ACTIVO' AND " +
+            "T2.id_departamento= ?1 ";
+    @Query(value =query2, nativeQuery = true)
+    public List<Provincia> listarProvinciasActivosDepartamento(int idDepartamento);
 }
