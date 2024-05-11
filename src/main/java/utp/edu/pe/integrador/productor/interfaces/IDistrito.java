a2c340d32173767c7c87360513ff9f1db0437b12
OscarCantoral <52679085+OscarCantoral@users.noreply.github.com>
2024-05-11 16:38:07 -0500

    Update IDistrito.java

--- src/main/java/utp/edu/pe/integrador/productor/interfaces/IDistrito.java
+++ src/main/java/utp/edu/pe/integrador/productor/interfaces/IDistrito.java
@@ -14,4 +14,18 @@ public interface IDistrito extends CrudRepository<Distrito, Integer> {
 
     @Query(value ="select * from distrito where estado_distrito = 'ACTIVO'", nativeQuery = true)
     public List<Distrito> listarDistritosActivos();
+
+    String query2=
+            "SELECT T1.* FROM distrito T1\n" +
+                    "INNER JOIN provincia T2 ON\n" +
+                    "T1.cod_provincia=T2.cod_provincia\n" +
+                    "INNER JOIN departamento T3 ON\n" +
+                    "T1.cod_departamento=T3.cod_departamento\n" +
+                    "WHERE\n" +
+                    "T3.estado_departamento='ACTIVO' AND\n" +
+                    "T2.estado_provincia='ACTIVO' AND\n" +
+                    "T1.estado_distrito='ACTIVO' AND\n" +
+                    "T2.id_provincia= ?1 ";
+    @Query(value =query2, nativeQuery = true)
+    public List<Distrito> listarDistritosActivosProvincia(int idProvincia);
 }
